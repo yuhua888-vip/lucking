ffeaa019e96458f56e75a9cad1cc84abce50f24a
yuhua888-vip <256897492@qq.com>
2026-05-06 07:08:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -356,3 +356,20 @@ function betCustom(side){
 
   bet(side, amount);
 }
+function betCustom(side){
+  let amount = 0;
+
+  if(side === "正面"){
+    amount = Number(document.getElementById("frontAmount").value);
+  }else{
+    amount = Number(document.getElementById("backAmount").value);
+  }
+
+  if(!amount || amount <= 0){
+    result.innerText = "请输入正确的下注金额";
+    soundLose();
+    return;
+  }
+
+  bet(side, amount);
+}
